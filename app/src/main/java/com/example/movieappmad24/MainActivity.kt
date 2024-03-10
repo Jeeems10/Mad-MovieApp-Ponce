2a62db0f567728f82480c3a807799f0bc8e52ef3
Jeeems10 <jamesponce10@outlook.com>
2024-03-10 17:32:37 +0100

    - First Task is working TopBar and BottomBar

--- app/src/main/java/com/example/movieappmad24/MainActivity.kt
+++ app/src/main/java/com/example/movieappmad24/MainActivity.kt
@@ -1,18 +1,63 @@
 package com.example.movieappmad24
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ShapeDefaults
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ModifierInfo
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.movieappmad24.models.Movie
+import com.example.movieappmad24.models.getMovies
 import com.example.movieappmad24.ui.theme.MovieAppMAD24Theme
 
 class MainActivity : ComponentActivity() {
@@ -25,11 +70,157 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Card {
-                        Image(painter = painterResource(id = R.drawable.movie_image), contentDescription = "placeholder_image")
-                    }
+                    App()
                 }
             }
         }
+    }
+}
+
+
+@Composable
+fun App(){
+    TopAndBottomBar()
+}
+
+data class NavItemState(
+    val title : String,
+    val selectedIcon : ImageVector,
+    val unselectedIcon : ImageVector
+)
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAndBottomBar(modifier: Modifier = Modifier){
+
+    val items = listOf(
+        NavItemState(
+            title = "Home",
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Outlined.Home
+        ),
+
+        NavItemState(
+            title = "Watchlist",
+            selectedIcon = Icons.Filled.Star,
+            unselectedIcon = Icons.Outlined.Star
+        )
+    )
+
+    var bottomNavState by rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {
+                Box(modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.Center){
+                    Text(text = "Movie App", color = MaterialTheme.colorScheme.primary)
+                }
+            })
+
+        },
+
+        bottomBar = {
+            NavigationBar (modifier
+                .clip(RoundedCornerShape(0.dp,0.dp,20.dp,20.dp))) {
+                items.forEachIndexed{index, item ->
+                    NavigationBarItem(
+                        selected = bottomNavState == index,
+                        onClick = { bottomNavState = index },
+                        icon = {
+                            Icon(
+                                imageVector = if(bottomNavState == index){
+                                    item.selectedIcon}
+                                else  {item.unselectedIcon},
+                                contentDescription = item.title)
+                        },
+                        label = {
+                            Text(text = item.title)
+                        }
+                    )
+                }
+            }
+        }
+
+    ) {  contentPadding ->
+        MovieList(movies = getMovies())
+    }
+}
+
+@Composable
+fun MovieList(movies: List<Movie> = getMovies()){
+    LazyColumn {
+        items(movies) { movie ->
+            MovieRow(movie)
+        }
+    }
+}
+
+
+@Composable
+fun MovieRow(movie: Movie){
+    var showDetails by remember {
+        mutableStateOf(false)
+    }
+
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .padding(5.dp),
+        shape = ShapeDefaults.Large,
+        elevation = CardDefaults.cardElevation(10.dp)
+    ) {
+        Column {
+            Box(
+                modifier = Modifier
+                    .height(150.dp)
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.movie_image),
+                    contentScale = ContentScale.Crop,
+                    contentDescription = "placeholder image")
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(10.dp),
+                    contentAlignment = Alignment.TopEnd
+                ){
+                    Icon(
+                        tint = MaterialTheme.colorScheme.secondary,
+                        imageVector = Icons.Default.FavoriteBorder,
+                        contentDescription = "Add to favorites")
+                }
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(6.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = movie.title)
+                Icon(modifier = Modifier
+                    .clickable {
+                        showDetails = !showDetails
+                    },
+                    imageVector =
+                    if (showDetails) Icons.Filled.KeyboardArrowDown
+                    else Icons.Default.KeyboardArrowUp, contentDescription = "show more")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DefaultPreview(){
+    MovieAppMAD24Theme {
+        TopAndBottomBar()
+        //MovieList(movies = getMovies())
     }
 }
